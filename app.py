4e4e6d4751b1e0f4b1303c2036ca1fd4971cdb29
Your Name <you@example.com>
2021-02-22 13:50:38 -0500

    commit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import spotipy
+import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
 
-lz_uri = 'spotify:artist:43ZHCT0cAZBISjO8DG9PnE' #jcole
 
-spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="88bb7350722b4d3898b677e75a9a3d75",
-                                                           client_secret="e8882c8fd78f4f5182130fe4c6f5110e"))
+lz_uri = 'spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ' #abel
+
+CLIENT_ID = "88bb7350722b4d3898b677e75a9a3d75"
+CLIENT_SECRET = "e8882c8fd78f4f5182130fe4c6f5110e"
+SCOPE = ""
+REDIRECT_URI = ""
+
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET))
 
 
 
@@ -12,7 +18,7 @@ tracks = spotify.artist_top_tracks(lz_uri)   #returns a json with "tracks" as th
 #the value of tracks in an array of tracks
 #in each element of the array... is the album where the track is, the name, the etc.....
 
-
+print(spotify.current_user())   
 for i in range (0,10):
 
     print(tracks['tracks'][i]['name'])
