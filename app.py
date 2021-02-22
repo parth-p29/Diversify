1ee1255a0c445fc12b9e3950f0e28bb8c05054f9
Your Name <you@example.com>
2021-02-22 13:57:42 -0500

    commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ lz_uri = 'spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ' #abel
 CLIENT_ID = "88bb7350722b4d3898b677e75a9a3d75"
 CLIENT_SECRET = "e8882c8fd78f4f5182130fe4c6f5110e"
 SCOPE = ""
-REDIRECT_URI = ""
+REDIRECT_URI = ""#test
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET))
 
@@ -19,6 +19,6 @@ tracks = spotify.artist_top_tracks(lz_uri)   #returns a json with "tracks" as th
 #in each element of the array... is the album where the track is, the name, the etc.....
 
 print(spotify.current_user())   
-for i in range (0,10):
+for i in range (0,5):
 
     print(tracks['tracks'][i]['name'])
