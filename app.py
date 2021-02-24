a9f7fd5382963bce2241b5354e07013ba69bf6c7
Your Name <parth.spatel871@gmail.com>
2021-02-24 10:28:17 -0500

    commit

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from secrets import client_id, client_secret, redirect_uri
-'''
-client_id="88bb7350722b4d3898b677e75a9a3d75"
-client_secret="e8882c8fd78f4f5182130fe4c6f5110e"
-redirect_uri="http://localhost:8888/callback/"
-'''
+from secrets import *
+
 class SpotifyCient:
 
     def __init__ (self):
