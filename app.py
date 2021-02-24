166d2c6ecd3db04e9d68d8d6d25a118700725f1b
Your Name <parth.spatel871@gmail.com>
2021-02-24 10:23:22 -0500

    commit

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from secrets import client_id, client_secret, redirect_uri
+'''
+client_id="88bb7350722b4d3898b677e75a9a3d75"
+client_secret="e8882c8fd78f4f5182130fe4c6f5110e"
+redirect_uri="http://localhost:8888/callback/"
+'''
+class SpotifyCient:
 
+    def __init__ (self):
+
+        self.CLIENT_ID = client_id
+        self.CLIENT_SECRET = client_secret
+        self.REDIRECT_URI = redirect_uri
+
+    def get_top_tracks(self, limit, time_range, scope):
+
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(self.CLIENT_ID, self.CLIENT_SECRET, self.REDIRECT_URI, scope))
+
+        results = sp.current_user_top_tracks(limit=limit, time_range=time_range)
+
+        for track_idx in range(limit):
+
+            print(results['items'][track_idx]['name'])
+
+
+        
+
+user = SpotifyCient()
+
+user.get_top_tracks(10, "long_term", "user-top-read")
+
+
+
+        
+
+'''
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="88bb7350722b4d3898b677e75a9a3d75",
                                                client_secret="e8882c8fd78f4f5182130fe4c6f5110e",
                                                redirect_uri="http://localhost:8888/callback/",
                                                scope="user-top-read"))  #the scope determines how much access you have of the user's info
                                                #different request endpoints require different scopes
+
+
+sp2 = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="88bb7350722b4d3898b677e75a9a3d75",
+                                               client_secret="e8882c8fd78f4f5182130fe4c6f5110e",
+                                               redirect_uri="http://localhost:8888/callback/",
+                                               scope="user-library-read"))
+
 
 results = sp.current_user_top_artists(limit=2, offset=0, time_range="long_term")
 
@@ -14,16 +56,13 @@ for track_idx in range(2):
     print(results['items'][track_idx]['name'])
 
 
-'''
+
 results = sp.current_user_top_tracks(limit=20, offset=0, time_range="medium_term")
 print(results)
 '''
+
+
 '''
-import spotipy
-import pandas as pd
-from spotipy.oauth2 import SpotifyClientCredentials
-
-
 lz_uri = 'spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ' #abel
 
 CLIENT_ID = "88bb7350722b4d3898b677e75a9a3d75"
